560878e411e506b979d5986a83f98098eeb5fe6a
travis.dilts@gmail.com <travis.dilts@gmail.com@40ceb232-ebdf-7284-4f65-65f8c3023b07>
2012-05-16 23:56:34 +0000

    This is broken

--- NameTag/gen/com/HalfAlpha/NameTag/R.java
+++ NameTag/gen/com/HalfAlpha/NameTag/R.java
@@ -8,23 +8,42 @@
 package com.HalfAlpha.NameTag;
 
 public final class R {
+    public static final class array {
+        public static final int display_stat=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int gdgtData=0x7f050002;
-        public static final int gdgtUser=0x7f050000;
-        public static final int refreshButton=0x7f050001;
+        public static final int CycleLabel=0x7f06000b;
+        public static final int autoRefresh=0x7f060004;
+        public static final int autoRefreshInterval=0x7f060005;
+        public static final int cycleInterval=0x7f06000a;
+        public static final int gdgtData=0x7f06000d;
+        public static final int gdgtUser=0x7f060002;
+        public static final int mainScroll=0x7f060000;
+        public static final int minutesLabel=0x7f060006;
+        public static final int refreshButton=0x7f060003;
+        public static final int showStatistic=0x7f060008;
+        public static final int textView1=0x7f060007;
+        public static final int textView2=0x7f060009;
+        public static final int textView3=0x7f06000c;
+        public static final int title=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int enterGdgtUser=0x7f040002;
-        public static final int gdgtData=0x7f040003;
-        public static final int hello=0x7f040000;
+        public static final int alert_dialog_no=0x7f050006;
+        public static final int alert_dialog_no_bt=0x7f050004;
+        public static final int alert_dialog_ok=0x7f050008;
+        public static final int alert_dialog_warning_title=0x7f050007;
+        public static final int alert_dialog_yes=0x7f050005;
+        public static final int app_name=0x7f050000;
+        public static final int autoRefresh=0x7f050003;
+        public static final int enterGdgtUser=0x7f050001;
+        public static final int gdgtData=0x7f050002;
     }
 }
--- NameTag/src/com/HalfAlpha/NameTag/NameTagActivity.java
+++ NameTag/src/com/HalfAlpha/NameTag/NameTagActivity.java
@@ -1,9 +1,19 @@
 package com.HalfAlpha.NameTag;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
@@ -17,6 +27,8 @@ import android.widget.ScrollView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class NameTagActivity extends Activity {
 	private static final int SEC_LENGTH_MS = 1000;
@@ -31,12 +43,32 @@ public class NameTagActivity extends Activity {
 	private EditText gdgtUser;
 	private CheckBox autoRefresh;
 	private ScrollView mainScrollView;
+	BluetoothAdapter mBluetoothAdapter;
+	TextView myLabel;
+	BluetoothDevice mmDevice;
+	BluetoothSocket mmSocket;
+	OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+    EditText myTextbox;
+
+
 	
 	private Runnable displayValues = new Runnable() {
 
 		@Override
 		public void run() {
 			//call Arduino with data
+			try {
+				sendData();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			cycleValues();
 		}
 	};
@@ -99,7 +131,15 @@ public class NameTagActivity extends Activity {
 		autoRefreshInterval = (EditText) findViewById(R.id.autoRefreshInterval);
 		cycle = (EditText) findViewById(R.id.cycleInterval);
 		mainScrollView = (ScrollView) findViewById(R.id.mainScroll);
-		
+		findBT();
+		try {
+			openBT();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		beginListenForData();
+
 		refreshButton.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
 				
@@ -184,4 +224,135 @@ public class NameTagActivity extends Activity {
 
 		return builder.create();
 	}
+	
+	public void finishDialogNoBluetooth() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.alert_dialog_no_bt)
+        .setIcon(android.R.drawable.ic_dialog_info)
+        .setTitle(R.string.app_name)
+        .setCancelable( false )
+        .setPositiveButton(R.string.alert_dialog_ok, new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       finish();            	
+                	   }
+               });
+        AlertDialog alert = builder.create();
+        alert.show(); 
+    }
+	
+	void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("FireFly-108B")) 
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+    }
+	
+	void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard //SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);        
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+
+        myLabel.setText("Bluetooth Opened");
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler(); 
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {                
+               while(!Thread.currentThread().isInterrupted() && !stopWorker)
+               {
+                    try 
+                    {
+                        int bytesAvailable = mmInputStream.available();                        
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            myLabel.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    } 
+                    catch (IOException ex) 
+                    {
+                        stopWorker = true;
+                    }
+               }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData() throws IOException
+    {
+        String msg = currentInfo.toString();
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        //mmOutputStream.write('A');
+        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        myLabel.setText("Bluetooth Closed");
+    }
 }
